78fe8e226ee11eca824d5441de85ca6a0b4661e5
baixue <baixuexue123@hotmail.com>
2017-07-06 21:58:40 +0800

    remove logconfig

--- handlers/foo.py
+++ handlers/foo.py
@@ -4,6 +4,10 @@ import logging
 
 logger = logging.getLogger('boilerplate.' + __name__)
 
+access_log = logging.getLogger("tornado.access")
+app_log = logging.getLogger("tornado.application")
+gen_log = logging.getLogger("tornado.general")
+
 
 class FooHandler(BaseHandler):
     def get(self):
