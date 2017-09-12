a140edcac53e2f979223a52fe4338e23ff9c5615
bxf_100_u <baixuexue123@hotmail.com>
2017-09-12 19:01:51 +0800

    tornado cocourine test

--- demos/demo5.py
+++ demos/demo5.py
@@ -7,29 +7,31 @@ import time
 
 @gen.coroutine
 def asyn_div(a, b):
-    print(f"--- begin: div {a}+{b} ---")
+    print("22222222222222222222")
     val = a / b
+    yield gen.sleep(2.0)
+    print("3333333333333333333")
     return val
 
 
 @gen.coroutine
 def asyn_sum(a, b):
-    print(f"--- begin: sum {a}+{b} ---")
+    print("1111111111111111111")
 
     _sum = a + b
 
     _div = yield asyn_div(_sum, b)
 
-    print("--- after yielded ---")
-    print(f"end: sum:{_sum} div:{_div}")
+    print("4444444444444444444")
 
 
 def main():
-    print('----------------------------')
+    print('------------ begin ----------------')
     future = asyn_sum(1, 2)
-    print('----------------------------')
-    # tornado.ioloop.IOLoop.current().add_future(future, lambda f: f.result())
-    tornado.ioloop.IOLoop.current().start()
+    print('------------- end ---------------')
+    io_loop = tornado.ioloop.IOLoop.current()
+    io_loop.add_future(future, lambda f: io_loop.stop())
+    io_loop.start()
 
 
 if __name__ == "__main__":
