08675432dce26dbae5c6bfdb892e38c727b3351c
bxf_100_u <baixuexue123@hotmail.com>
2017-09-14 17:04:01 +0800

    PeriodicCallback

--- demos/demo4.py
+++ demos/demo4.py
@@ -1,30 +1,38 @@
-from urllib.request import urlopen
-
 from tornado import gen
-from tornado.ioloop import IOLoop
+from tornado.ioloop import IOLoop, PeriodicCallback
+from tornado.httpclient import AsyncHTTPClient
 
 
 @gen.coroutine
-def handle_response(resp):
-    print(resp)
-    return resp
-
-
-@gen.coroutine
-def fetch(url):
-    print('fetch: 1111111111111111')
-    resp = urlopen(url)
-    print('fetch: 2222222222222222')
-    resp = yield handle_response(resp)
-    return resp
+def fetch(urls):
+    http = AsyncHTTPClient()
+    resps = yield list(map(http.fetch, urls))
+    return resps
 
 
 if __name__ == '__main__':
-    print('start')
+    import pprint
+    urls = [
+        'http://python.jobbole.com/',
+        'http://www.baidu.com/',
+        'http://www.sohu.com/',
+        'http://www.sina.com/',
+        'http://www.ruanyifeng.com',
+        'http://cnodejs.org/',
+        'http://www.pythontab.com/',
+        'http://docs.jinkan.org/docs/jinja2/',
+        'https://www.djangoproject.com/start/overview/',
+        'http://www.semantic-ui.cn/',
+    ]
+    future = fetch(urls)
 
-    future = fetch('http://python.jobbole.com/')
-
-    print('end')
     io_loop = IOLoop.current()
     io_loop.add_future(future, lambda f: io_loop.stop())
+
+    def callback():
+        pprint.pprint(io_loop._handlers)
+
+    period = PeriodicCallback(callback=callback, callback_time=200, io_loop=io_loop)
+    period.start()
+
     io_loop.start()
